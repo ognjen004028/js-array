d55dad0991c1093ae61cd06fce2d9c19e29b4314
Ognjen <160677447+ognjen004028@users.noreply.github.com>
2024-03-09 15:26:33 +0100

    Add files via upload

--- js-array/array.js
+++ js-array/array.js
@@ -3,23 +3,31 @@ function addTxt(){
     const input = fragment
         .appendChild(document.createElement("p"))
         .appendChild(document.createElement("input"))
-    input.type = "number"; 
+    input.type = "text"; 
 
     document.body.appendChild(fragment);
 }
 
 function getArray(){
-    let numElems = document.getElementsByTagName('input').length;
-    let array = [];
+    /**  let numElems = document.getElementsByTagName('input').length;
+    // let array = [];
 
-    for (let i = 0; i < numElems; i++) {
-            if(document.getElementsByTagName('input')[i].value == ''){
-                continue;
-            }
-                array.push(
-                    document.getElementsByTagName('input')[i].value);  
-            }
-    return array;
+    // for (let i = 0; i < numElems; i++) {
+    //         if(document.getElementsByTagName('input')[i].value == ''){
+    //             continue;
+    //         }
+    //             array.push(
+    //                 document.getElementsByTagName('input')[i].value);  
+    //         }
+    // return array; */
+
+    const inputs = document.getElementsByTagName('input');
+    const myArray = Array.from(inputs)
+                    .filter(input => input.value !== '' && !isNaN(input.value))
+                    .map(input => parseInt(input.value));
+
+    return myArray;
+    
 }
 
 function sortArray(){
@@ -44,7 +52,7 @@ function sortArray(){
 }
 
 function sumEven() {
-    let sum = 0;
+    /**let sum = 0;
     const array = getArray();
 
     array.forEach(element => {
@@ -53,5 +61,17 @@ function sumEven() {
         }
     });
 
-    alert(+sum);
+    alert(+sum);*/
+    
+    const array = getArray();
+
+    const sum = array.reduce((accumulator, currentValue) => {
+        if (currentValue % 2 === 0 && currentValue > 0) {
+            return accumulator + parseFloat(currentValue);
+        } else {
+            return accumulator;
+        }
+    }, 0);
+
+    alert(sum);
 }
